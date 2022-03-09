b63aaba13536a52920d3d5cbe1af05ff7587f3ff
Fernando Gomez <shadyfast8.services@gmail.com>
2022-03-09 11:18:48 +0100

    Make the module 3 assignment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel';
 import Board from '../Board';
-import { createTiles } from '../../misc/utils';
+import { createTiles, indexOfSelected } from '../../misc/utils';
 
 import './App.css';
 
@@ -19,11 +19,46 @@ class App extends Component{
   }
 
   startGame(numTiles) {
-    this.setState({
-      playing: true,
-      previousTileIndex: null,
-      toBeCleared: null,
-      tiles: createTiles(this.state.numTiles)
+    this.setState(function (state) {
+      return {
+        playing: true,
+        previousTileIndex: null,
+        toBeCleared: null,
+        tiles: createTiles(state.numTiles, this.handleTileClicked),
+      };
+    });
+  }
+
+  handleTileClicked(id, color) {
+    this.setState((state) => {
+      const { tiles } = state;
+      let { toBeCleared, previousTileIndex } = state;
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
+      }
+      tiles[selectedTileIndex].selected = true;
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex];
+        const selectedTile = tiles[selectedTileIndex];
+        if (
+          previousTile.id !== selectedTile.id &&
+          previousTile.color === color
+        ) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else {
+        previousTileIndex = selectedTileIndex;
+      }
+      return { tiles, toBeCleared, previousTileIndex };
     });
   }
 
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -7,7 +7,7 @@ const Tile = (props) => {
   const setColor = () => props.selected || props.matched ? { backgroundColor: props.color } : null
 
   return (
-    <div className='Tile' style={setColor()}>
+    <div className='Tile' style={setColor()} onClick={() => props.handleTileClicked(props.id, props.color)} >
       { props.selected || props.matched ? <props.svg /> : null }
     </div>
   )
